6e170fd7e65f0f00be83c1f1de84bb6befe53e82
skhacker105 <skhacker105@gmail.com>
2025-12-16 11:27:41 +0530

    added seeder function call on first api call

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,27 @@ const router = express.Router();
 
 import userRoute from './user.route';
 import authRoute from './auth.routes';
+
+const seedRoles = require("../seed/role.seeder");
+const seedPermissions = require("../seed/permission.seeder");
+const seedFeatures = require("../seed/feature.seeder");
+const seedMenus = require("../seed/menus.seeder");
+const seedRoleMenu = require("../seed/roleMenus.seeder");
+
 /**
  * Function contains Application routes
  *
  * @returns router
  */
 const routes = () => {
-  router.get('/', (req, res) => {
+  router.get('/', async (req, res) => {
+    
+    await seedRoles();
+    await seedPermissions();
+    await seedFeatures();
+    await seedMenus();
+    await seedRoleMenu();
+
     res.json('Welcome');
   });
   router.use('/users', userRoute);
