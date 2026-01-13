274a63b5b4563d2a3bfeeb4f1c1d8e4ec6474b9f
skhacker105 <skhacker105@gmail.com>
2026-01-13 22:51:58 +0530

    code changes

--- src/index.js
+++ src/index.js
@@ -21,37 +21,19 @@ const host = process.env.APP_HOST;
 const port = process.env.APP_PORT;
 const api_version = process.env.API_VERSION;
 
-app.use(
-  cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'X-Requested-With',
-      'x-fcm-token'
-    ],
-    credentials: false
-  })
-);
-
-app.options('*', cors());
-// app.use(helmet({
-//   crossOriginResourcePolicy: false,
-//   crossOriginEmbedderPolicy: false,
-//   crossOriginOpenerPolicy: false
-// }));
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','PUT','DELETE','OPTIONS','PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(morgan('combined', { stream: logStream }));
 
 database();
-app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
+
 app.use(`/api/${api_version}`, routes());
 app.use(appErrorHandler);
 app.use(genericErrorHandler);
