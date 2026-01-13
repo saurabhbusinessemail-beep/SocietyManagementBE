f21a3c5d928a597003675fa9aa05dff5c2725c29
skhacker105 <skhacker105@gmail.com>
2026-01-14 00:06:28 +0530

    code changes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -20,7 +20,7 @@ router.put('/:_id', userAuth, userController.updateUser);
 router.patch('/updateName', userAuth, userController.updateName);
 
 //route to update fcm token by their user id
-router.patch('/updateFCMToken', userAuth, userController.updateFCMToken);
+router.post('/updateFCMToken', userAuth, userController.updateFCMToken);
 
 //route to delete a single user by their user id
 router.delete('/:_id', userAuth, userController.deleteUser);
