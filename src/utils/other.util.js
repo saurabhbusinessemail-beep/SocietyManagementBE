e2594e78f50ebc0b98d75bb915209fa87a8e439e
skhacker105 <skhacker105@gmail.com>
2026-01-17 14:21:00 +0530

    date filter using ITC time offset

--- src/utils/other.util.js
+++ src/utils/other.util.js
@@ -3,11 +3,11 @@ export const getISTDayRange = (dateInput = new Date()) => {
   const IST_OFFSET = 5.5 * 60 * 60 * 1000; // 5.5 hours in milliseconds
 
   // Create date at 00:00:00 IST
-  const startOfDayIST = new Date(date);
+  const startOfDayIST = new Date(dateInput);
   startOfDayIST.setHours(0, 0, 0, 0);
 
   // Create date at 23:59:59.999 IST
-  const endOfDayIST = new Date(date);
+  const endOfDayIST = new Date(dateInput);
   endOfDayIST.setHours(23, 59, 59, 999);
 
   // Convert to UTC
